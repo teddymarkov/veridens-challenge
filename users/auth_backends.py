700ed9ee331f96b7c9a022949da7160c3a6851ab
Teodor Markov <mail@teddymarkov.com>
2018-05-28 20:31:35 +0300

    Email authentication added

--- users/auth_backends.py
+++ users/auth_backends.py
@@ -1,5 +1,33 @@
 # coding=utf-8
 
+from users.models import User
+
+
 class EmailAuthBackend(object):
-    # TODO: Enable users to login with email
-    pass
+    """
+    Email Authentication Backend
+
+    Allows a user to sign in using an email/password pair, then check
+    a username/password pair if email failed
+    """
+    def authenticate(self, username=None, password=None, **kwargs):
+        """ Authenticate a user based on email address as the user name. """
+        try:
+            user = User.objects.get(email=username)
+            if getattr(user, 'is_active') and user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            try:
+                user = User.objects.get(username=username)
+                if getattr(user, 'is_active') and \
+                        user.check_password(password):
+                    return user
+            except User.DoesNotExist:
+                return None
+
+    def get_user(self, user_id):
+        """ Get a User object from the user_id. """
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
--- users/models.py
+++ users/models.py
@@ -3,5 +3,70 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# TODO: Create substitute user model
-# Hint: https://docs.djangoproject.com/en/1.9/topics/auth/customizing/#substituting-a-custom-user-model
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+# https://docs.djangoproject.com/en/1.9/topics/auth/customizing/#substituting-a-custom-user-model
+
+USERNAME_REGEX = '^[a-z+-]*$'
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(username=username,
+                          email=self.normalize_email(email))
+        user.is_active = True
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, username, email, is_admin, is_staff,
+                         password=None):
+        user = self.create_user(username, email, password)
+        user.is_admin = is_admin
+        user.is_staff = is_staff
+        user.save(using=self._db)
+
+        return user
+
+    def get_by_natural_key(self, username):
+        return self.get(**{self.model.USERNAME_FIELD: username})
+
+
+class User(AbstractBaseUser):
+
+    username = models.CharField(max_length=40, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(max_length=300,
+                              unique=True,
+                              verbose_name='email address')
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'is_admin', 'is_staff']
+
+    def __str__(self):
+        return self.username
+
+    def get_short_name(self):
+        # The user is identified by their username
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        """Does the user have a specific permission?"""
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        """Does the user have permissions to view the app {{}}?"""
+        # Simplest possible answer: Yes, always
+        return True
